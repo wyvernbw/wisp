f9382f18aa19955ca22406a29204cff59021a927
wyvernbw <iamwyvern511@gmail.com>
2025-07-26 00:21:05 +0300

    add effect api

--- wisp.gd
+++ wisp.gd
@@ -5,11 +5,91 @@ extends Node
 class State:
 	extends Resource
 	signal transition(state)
+	signal new_effect(id)
 
 	func name() -> String:
 		return "Unnamed"
 	
 	var valid := false
+	var current_effect := -1
+
+	func consume_effect() -> State:
+		current_effect = -1
+		return self
+
+	func recv_effect(id: int) -> int:
+		while yield(self, "new_effect") != id:
+			pass
+		return id
+
+	func send_effect(id: int) -> State:
+		self.current_effect = id
+		self.emit_signal("new_effect", id)
+		return self
+
+	func poll_effect(id: int, value: bool) -> bool:
+		if self.current_effect != -1:
+			if self.current_effect == id:
+				self.consume_effect()
+				return true
+		return value
+
+	class EffectResult:
+		enum EffectStatus {
+			Done,
+			HasData		
+		}
+		var inner := {}
+
+		func _init(value: Dictionary) -> void:
+			assert(value.keys().size() == 1)
+			self.inner = value
+
+		func key() -> int:
+			return self.inner.keys()[0]
+			
+		func is_done() -> bool:
+			return self.key() == EffectStatus.Done
+
+		func has_data() -> bool:
+			return self.key() == EffectStatus.HasData
+
+		func data():
+			return self.inner.get(EffectStatus.HasData, null)
+
+		func data_unchecked():
+			return self.inner[EffectStatus.HasData]
+
+	func await_effect_result(id: int, value: GDScriptFunctionState) -> EffectResult:
+		var res = WispSelector.new().select([
+			self.recv_effect(id),
+			value,
+		])
+		match yield(res, "completed"):
+			{ 0: id }:
+				self.consume_effect()
+				return EffectResult.new({
+					EffectResult.EffectStatus.Done: id		
+				})
+			{ 0: _ }:
+				assert(false, "unreachable")
+
+			{ 1: null }:
+				return EffectResult.new({
+					EffectResult.EffectStatus.Done: id
+				})
+			{ 1: var data }:
+				return EffectResult.new({
+					EffectResult.EffectStatus.HasData: data
+				})
+		return false
+
+	func await_effect(id: int, value: GDScriptFunctionState, default = null):
+		var res = yield(await_effect_result(id, value), "completed")
+		if res.has_data():
+			return res.data_unchecked()
+		return default
+		
 
 	func enter(owner) -> Wisp.State:
 		return self
@@ -152,6 +232,10 @@ class StateMachine:
 			return "disabled"
 		else:
 			return current_state.name()
+
+	func send_effect(id: int) -> StateMachine:
+		self.current_state.send_effect(id)
+		return self
 		
 static func use_state_machine(owner, initial_state: State) -> StateMachine:
 	return StateMachine.create(owner, initial_state)
@@ -175,3 +259,54 @@ class Reducer:
 
 	func handle_command(entity, command: Command) -> State:
 		return self
+
+class WispSelector:
+	extends Reference
+	signal promise_completed
+
+	var done = false
+
+	static func wrap(object: Object, signal_name: String):
+		return yield(object, signal_name)
+
+	func select(
+		promises: Array
+	) -> Dictionary:
+		var signal_map = {}
+		var idx = 0
+		var promise_refs := []
+		for promise in promises:
+			assert(promise is GDScriptFunctionState, promise)
+			signal_map[idx] = {
+				"finished": false,
+				"value": null
+			}
+			var p = _promise(promise)
+			promise_refs.append(p)
+			p.connect(
+				'completed', self, '_on_promise_completed', [signal_map, idx], CONNECT_ONESHOT | CONNECT_REFERENCE_COUNTED
+			)
+			idx += 1
+		yield(self, "promise_completed")
+		done = true	
+		for branch in signal_map.keys():
+			if signal_map[branch].finished:
+				return { branch: signal_map[branch].value }
+		return {}
+
+	func _on_promise_completed(value, signal_map, idx: int):
+		if done:
+			return
+		signal_map[idx] = {
+			"finished": true,
+			"value": value
+		}
+		emit_signal("promise_completed")
+
+		
+	static func _promise(state: GDScriptFunctionState):
+		var res = yield(state, "completed")
+		if res == null:
+			return Maybe.new()
+		else:
+			return res
